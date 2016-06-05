d68f22146911a184828496f162e76c72d9a02a56
Tomáš Syrový <syrovy.tom@gmail.com>
2016-06-05 12:55:53 +0200

    Add gitignore, update composer, add base class.

---
+++ ContentEditableBundle.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * Project: contenteditable-bundle
+	 * File: ContentEditableBundle.php
+	 * Author: Tomas Syrovy <syrovy.tom@gmail.com>
+	 * Date: 05.06.16
+	 * Version: 1.0
+	 */
+
+	namespace tomsyrovy\ContentEditableBundle;
+
+	use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+	/**
+	 * Class ContentEditableBundle
+	 * @package tomsyrovy\ContentEditableBundle
+	 */
+	class ContentEditableBundle extends Bundle{
+
+	}
